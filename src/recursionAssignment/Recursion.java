95c2093f04dcee55513d789a04d89f09b609a925
iydia <lydia.he23@ycdsbk12.ca>
2023-01-11 23:29:26 -0500

    Problem set ONE

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -1,6 +1,17 @@
 package recursionAssignment;
 
 public class Recursion{
-
-
+    
+    // Problem set ONE
+    static int count = 0;
+    public static int count7(int n){
+        if(n<=0){
+            return count;
+        }
+        else if(n%10 == 7){
+            count+=1;
+        }
+        count7(n/10); 
+        return count;
+    }
 }
